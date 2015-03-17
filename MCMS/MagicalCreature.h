7633e650582af6a8741ad298def159ff34121fcd
Sevy11 <michaelsevy@gmail.com>
2015-03-17 14:08:32 -0500

    first commit

---
+++ MCMS/MagicalCreature.h
@@ -0,0 +1,15 @@
+//
+//  MagicalCreature.h
+//  MCMS
+//
+//  Created by Michael Sevy on 3/17/15.
+//  Copyright (c) 2015 Michael Sevy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MagicalCreature : NSObject
+
+@property NSString *name;
+
+@end
---
+++ MCMS/MagicalCreature.m
@@ -0,0 +1,13 @@
+//
+//  MagicalCreature.m
+//  MCMS
+//
+//  Created by Michael Sevy on 3/17/15.
+//  Copyright (c) 2015 Michael Sevy. All rights reserved.
+//
+
+#import "MagicalCreature.h"
+
+@implementation MagicalCreature
+
+@end
